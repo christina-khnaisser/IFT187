61b21cae8ead2d82ed63c2afa294d8db16fdacf1
Christina Khnaisser <Christina.Khnaisser@usherbrooke.ca>
2026-04-09 14:12:00 -0400

    Ajout TD07

---
+++ TD/TD07/test/SSAQ-table-ins_test.sql
@@ -0,0 +1,29 @@
+/*
+============================================================================== A
+Produit : CoFELI.Exemple.Universite
+Responsable : Christina.Khnaisser@usherbrooke.ca
+Version : 1.0.0a (2026-01-11)
+Statut : en développement
+Encodage : UTF-8, sans BOM; fin de ligne simple (LF)
+Plateformes : ISO, PostgreSQL
+============================================================================== A
+*/
+
+--
+/*
+-- =========================================================================== Z
+Contributeurs :
+  (CK) Christina.Khnaisser@USherbrooke.ca
+
+Tâches projetées :
+TODO 2026-01-11 (CK) : Compléter le code
+
+Tâches réalisées :
+  2026-01-11 (CK) : Création
+
+Référence :
+  [IFT187] https://github.com/christina-khnaisser/IFT187
+-- -----------------------------------------------------------------------------
+-- Fin de SSAQ-table-ins_cre.sql
+-- =========================================================================== Z
+*/
